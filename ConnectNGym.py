d9879575ed64b0af40727a8f10b2fce6b5a75b74
Frank <dingding303@gmail.com>
2020-07-25 10:50:12 +0800

    tab 2 files

--- ConnectNGym.py
+++ ConnectNGym.py
@@ -5,77 +5,80 @@ import gym
 from gym import spaces
 
 from PyGameConnectN import PyGameBoard
+from connect_n import ConnectNGame
 
 REWARD_A = 1
 REWARD_B = -1
 REWARD_TIE = 0
 REWARD_NONE = None
 
+
 class ConnectNGym(gym.Env):
 
-    def __init__(self, grid_num=3, connect_num=3):
-        self.grid_num = grid_num
-        self.connect_num = connect_num
-        self.action_space = spaces.Discrete(grid_num* grid_num)
-        self.observation_space = spaces.Discrete(grid_num * grid_num)
+	def __init__(self, board_size=3, connect_num=3):
+		self.board_size = board_size
+		self.connect_num = connect_num
+		self.action_space = spaces.Discrete(board_size * board_size)
+		self.observation_space = spaces.Discrete(board_size * board_size)
 
-        # self.boardGame = PyGameBoard(board_size=grid_num, connect_num=connect_num)
+		# self.boardGame = PyGameBoard(board_size=grid_num, connect_num=connect_num)
 
-        self.seed()
-        # self.reset()
+		self.seed()
+		# self.reset()
 
-    def reset(self) -> PyGameBoard:
-        self.boardGame = PyGameBoard(board_size=self.grid_num, connect_num=self.connect_num)
-        # return copy.deepcopy(self.boardGame.connectNGame)
-        return self.boardGame
+	def reset(self) -> PyGameBoard:
+		connectNGame = ConnectNGame(board_size=self.board_size, N=self.connect_num)
+		self.boardGame = PyGameBoard(connectNGame)
+		# return copy.deepcopy(self.boardGame.connectNGame)
+		return self.boardGame
 
-    def step(self, action):
-        """Step environment by action.
+	def step(self, action):
+		"""Step environment by action.
 
-        Args:
-            action (int): Location
+		Args:
+			action (int): Location
 
-        Returns:
-            list: Obeservation
-            int: Reward
-            bool: Done
-            dict: Additional information
-        """
-        # assert self.action_space.contains(action)
+		Returns:
+			list: Obeservation
+			int: Reward
+			bool: Done
+			dict: Additional information
+		"""
+		# assert self.action_space.contains(action)
 
-        r, c = action
-        # loc = action
-        # if self.done:
-        #     return self._get_obs(), 0, True, None
+		r, c = action
+		# loc = action
+		# if self.done:
+		#     return self._get_obs(), 0, True, None
 
-        reward = REWARD_NONE
-        # place
-        result = self.boardGame.move(r, c)
-        if self.boardGame.isGameOver():
-            reward = result
+		reward = REWARD_NONE
+		# place
+		result = self.boardGame.move(r, c)
+		if self.boardGame.isGameOver():
+			reward = result
 
-        return copy.deepcopy(self.boardGame.connectNGame), reward, not result is None, None
+		return copy.deepcopy(self.boardGame.connectNGame), reward, not result is None, None
 
-    def render(self, mode='human', close=False):
-        self.action = self.boardGame.next_user_input()
-        return self.action
+	def render(self, mode='human', close=False):
+		self.action = self.boardGame.next_user_input()
+		return self.action
 
-        # if close:
-        #     return
-        # if mode == 'human':
-        #     # self._show_board(print)  # NOQA
-        #     print('')
-        # else:
-        #     pass
-        #     # self._show_board(logging.info)
-        #     # logging.info('')
+		# if close:
+		#     return
+		# if mode == 'human':
+		#     # self._show_board(print)  # NOQA
+		#     print('')
+		# else:
+		#     pass
+		#     # self._show_board(logging.info)
+		#     # logging.info('')
 
-    def get_available_actions(self):
-        return self.boardGame.getAvailablePositions()
+	def get_available_actions(self):
+		return self.boardGame.getAvailablePositions()
 
-    def show_board(self, gui=False, sec=2):
-        if not gui:
-            self.boardGame.connectNGame.drawText()
-            time.sleep(sec)
-        else:
-            self.boardGame.display(sec=sec)
+	def show_board(self, gui=False, sec=2):
+		if not gui:
+			self.boardGame.connectNGame.drawText()
+			time.sleep(sec)
+		else:
+			self.boardGame.display(sec=sec)
--- base_agent.py
+++ base_agent.py
@@ -9,66 +9,70 @@ from strategy import MinimaxStrategy, Strategy
 
 
 class BaseAgent(object):
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
-    def act(self, game:PyGameBoard, available_actions):
-        return random.choice(available_actions)
+	def act(self, game: PyGameBoard, available_actions):
+		return random.choice(available_actions)
+
 
 class AIAgent(BaseAgent):
-    def __init__(self, strategy: Strategy):
-        self.strategy = strategy
+	def __init__(self, strategy: Strategy):
+		self.strategy = strategy
 
-    def act(self, game: PyGameBoard, available_actions):
-        result, move = self.strategy.action(game.connectNGame)
-        assert move in available_actions
-        return move
+	def act(self, game: PyGameBoard, available_actions):
+		result, move = self.strategy.action(game.connectNGame)
+		assert move in available_actions
+		return move
+
 
 class HumanAgent(BaseAgent):
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
-    def act(self, game: PyGameBoard, available_actions):
-        return game.next_user_input()
+	def act(self, game: PyGameBoard, available_actions):
+		return game.next_user_input()
+
 
 def play_human_vs_human(env: ConnectNGym):
-    play(env, HumanAgent(), AIAgent(MinimaxStrategy()))
+	play(env, HumanAgent(), AIAgent(MinimaxStrategy()))
+
 
 def play_human_vs_ai(env: ConnectNGym):
-    pygameBoard: PyGameBoard = env.reset()
-    plannedMinimaxAgent = AIAgent(PlannedMinimaxStrategy(pygameBoard.connectNGame))
-    play(env, HumanAgent(), plannedMinimaxAgent)
+	pygameBoard: PyGameBoard = env.reset()
+	plannedMinimaxAgent = AIAgent(PlannedMinimaxStrategy(pygameBoard.connectNGame))
+	play(env, HumanAgent(), plannedMinimaxAgent)
+
 
 def play_ai_vs_ai(env: ConnectNGym):
-    pygameBoard: PyGameBoard = env.reset()
-    plannedMinimaxAgent = AIAgent(PlannedMinimaxStrategy(pygameBoard.connectNGame))
-    play(env, plannedMinimaxAgent, plannedMinimaxAgent)
+	pygameBoard: PyGameBoard = env.reset()
+	plannedMinimaxAgent = AIAgent(PlannedMinimaxStrategy(pygameBoard.connectNGame))
+	play(env, plannedMinimaxAgent, plannedMinimaxAgent)
+
 
 def play(env: ConnectNGym, agent1: BaseAgent, agent2: BaseAgent):
-    agents = [agent1, agent2]
+	agents = [agent1, agent2]
 
-    while True:
-        pygameBoard: PyGameBoard = env.reset()
-        done = False
-        env.show_board(True)
-        agent_id = -1
-        while not done:
-            agent_id = (agent_id + 1) % 2
-            available_actions = env.get_available_actions()
-            agent = agents[agent_id]
-            action = agent.act(pygameBoard, available_actions)
-            _, reward, done, info = env.step(action)
-            env.show_board(True)
+	while True:
+		pygameBoard: PyGameBoard = env.reset()
+		done = False
+		env.show_board(True)
+		agent_id = -1
+		while not done:
+			agent_id = (agent_id + 1) % 2
+			available_actions = env.get_available_actions()
+			agent = agents[agent_id]
+			action = agent.act(pygameBoard, available_actions)
+			_, reward, done, info = env.step(action)
+			env.show_board(True)
 
-            if done:
-                print(f'result={reward}')
-                time.sleep(3)
-                break
+			if done:
+				print(f'result={reward}')
+				time.sleep(3)
+				break
 
 
 if __name__ == '__main__':
-    env = ConnectNGym(grid_num=3, connect_num=3)
-    # play_ai_vs_ai(env)
-    play_human_vs_ai(env)
-
-
+	env = ConnectNGym(board_size=3, connect_num=3)
+	# play_ai_vs_ai(env)
+	play_human_vs_ai(env)
