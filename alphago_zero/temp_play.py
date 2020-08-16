5593f951f08c325fbd409b3bd5f18293bc9d55b4
Frank Ding <dingding303@gmail.com>
2020-08-16 21:25:31 +0800

    temp_play.py

--- alphago_zero/temp_play.py
+++ alphago_zero/temp_play.py
@@ -4,32 +4,39 @@ from collections import defaultdict
 from ConnectNGame import ConnectNGame
 from ConnectNGym import ConnectNGym
 from PyGameConnectN import PyGameBoard
-from agent import play
+from agent import play, AIAgent
 from alphago_zero.MCTSRolloutPlayer import MCTSRolloutPlayer
 from alphago_zero.mcts_pure import MCTSPlayer
+from minimax.PlannedStrategy import PlannedMinimaxStrategy
 
 
-def battle(n_games=10):
-    """
-    Evaluate the trained policy by playing against the pure MCTS player
-    Note: this is only for monitoring the progress of training
-    """
-    initial_game = ConnectNGame(board_size=4, n=3)
-    mcts_rollout_player = MCTSRolloutPlayer(playout_num=1000)
-    mcts_pure_player = MCTSPlayer(c_puct=5, n_playout=1000)
+def battle(initial_game: ConnectNGame, player_first, player_second, n_games=10):
     win_counts = defaultdict(int)
     board = PyGameBoard(connect_n_game=copy.deepcopy(initial_game))
     env = ConnectNGym(board, display_milli_sec=100)
     for i in range(n_games):
-        winner = play(env, mcts_rollout_player, mcts_pure_player, render=False)
+        winner = play(env, player_first, player_second, render=False)
         win_counts[winner] += 1
     print(f'rollout first: win: {win_counts[1]}, lose: {win_counts[2]}, tie:{win_counts[-1]}')
     for i in range(n_games):
-        winner = play(env, mcts_pure_player, mcts_rollout_player, render=False)
+        winner = play(env, player_second, player_first, render=False)
         win_counts[winner] += 1
     win_ratio = 1.0*(win_counts[1] + 0.5*win_counts[-1]) / n_games
     print(f'pure first: win: {win_counts[1]}, lose: {win_counts[2]}, tie:{win_counts[-1]}')
     return win_ratio
 
+def mcts_play():
+    initial_game = ConnectNGame(board_size=4, n=3)
+    mcts_rollout_player = MCTSRolloutPlayer(playout_num=1000)
+    mcts_pure_player = MCTSPlayer(c_puct=5, n_playout=1000)
+    battle(initial_game, mcts_rollout_player, mcts_pure_player, n_games=20)
+
+def minimax_mcts():
+    initial_game = ConnectNGame(board_size=4, n=3)
+    planned_minimax_agent = AIAgent(PlannedMinimaxStrategy(initial_game))
+    mcts_rollout_player = MCTSRolloutPlayer(playout_num=1000)
+    battle(initial_game, planned_minimax_agent, mcts_rollout_player, n_games=20)
+
 if __name__ == '__main__':
-    battle(n_games=20)
+    # mcts_play()
+    minimax_mcts()
