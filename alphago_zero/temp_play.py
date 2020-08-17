a4670154a835770a38066d38400df9d89e57500d
Frank Ding <dingding303@gmail.com>
2020-08-17 16:44:06 +0800

    battle bug fix

--- alphago_zero/temp_play.py
+++ alphago_zero/temp_play.py
@@ -17,12 +17,14 @@ def battle(initial_game: ConnectNGame, player_first, player_second, n_games=10):
     for i in range(n_games):
         winner = play(env, player_first, player_second, render=False)
         win_counts[winner] += 1
-    print(f'rollout first: win: {win_counts[1]}, lose: {win_counts[2]}, tie:{win_counts[-1]}')
+    print(f'first: win: {win_counts[1]}, lose: {win_counts[2]}, tie:{win_counts[-1]}')
     for i in range(n_games):
         winner = play(env, player_second, player_first, render=False)
-        win_counts[winner] += 1
+        win_counts[-winner] += 1
     win_ratio = 1.0*(win_counts[1] + 0.5*win_counts[-1]) / n_games
-    print(f'pure first: win: {win_counts[1]}, lose: {win_counts[2]}, tie:{win_counts[-1]}')
+    print(f'total {win_counts}')
+    print(f'second first: win: {win_counts[1]}, lose: {win_counts[2]}, tie:{win_counts[-1]}')
+
     return win_ratio
 
 def mcts_play():
